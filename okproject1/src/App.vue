bcc2c6cc0a971feaed8b224e9855de3c9dfaab28
Vince <archsboi5@protonmail.com>
2021-06-27 17:13:07 +0700

    structureOfProject

---
+++ okproject1/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Header/>
+    <Body msg="Welcome to Your Vue.js App"/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from './components/header.vue'
+import Body from './components/body.vue'
+import Footer from './components/footer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Body,
+    Footer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
